1b6e099b420fc15d72dcdf6148fc401d5ad198db
kietn20 <kietnguyen3698@gmail.com>
2023-05-07 12:40:58 -0700

    dynamic pages for each item

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import engine, load_items_from_db
+from database import engine, load_items_from_db, load_item_from_db
 
 app = Flask(__name__)
 
@@ -12,6 +12,14 @@ def home():
 def list_items():
     items = load_items_from_db()
     return jsonify(items)
+
+@app.route("/item/<id>")
+def show_item(id):
+    item = load_item_from_db(id)
+    if not item:
+        return "Not Found", 404
+    return render_template("itempage.html", item=item)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -20,5 +20,15 @@ def load_items_from_db():
         result = conn.execute(text("select * from items"))
         items = []
         for row in result.all():
-            items.append(row._mapping)
+            items.append(dict(row._mapping))
         return items
+    
+def load_item_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(text(f"SELECT * FROM items WHERE id = {id}"))
+        row = result.all()
+        if len(row) == 0:
+            return None
+        else:
+            return dict(row[0]._mapping)
+        
